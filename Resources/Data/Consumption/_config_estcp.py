105e9f7064b3498041b387b189fcd448dadb472b
hcasperfu <hcasperfu@lbl.gov>
2024-04-18 09:54:00 -0700

    updated config file

--- Resources/Data/Consumption/_config_estcp.py
+++ Resources/Data/Consumption/_config_estcp.py
@@ -22,7 +22,8 @@ dfBldg = pd.read_csv('buildings.csv',
                      dtype = {'bldg_no' : str,
                               'name' : str,
                               'gross_area_sf' : float,
-                              'gross_area_m2' : float})
+                              'gross_area_m2' : float,
+                              'remark1' : str})
 sBuis = dfBldg['bldg_no'].tolist()
 iCols = [16, 30, 31, 32]
     # column numbers of the respective utilities
