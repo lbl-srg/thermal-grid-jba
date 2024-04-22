f9f8ff7f3b238345c1a9df348543889f9b7bcff5
hcasperfu <hcasperfu@lbl.gov>
2024-04-22 16:18:18 -0700

    merged pdf figures

---
+++ Resources/Data/Consumption/mergeFigures.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 22 15:25:44 2024
+
+@author: casper
+
+This script merges pdf figures to a single file.
+"""
+
+import glob
+import os
+
+from _config_estcp import *
+from PyPDF2 import PdfMerger
+
+m = PdfMerger()
+pdfs = [a for a in sorted(glob.glob(os.path.join(dirFigu,"*.pdf")))]
+[m.append(pdf) for pdf in pdfs]
+with open("figures_merged.pdf", "wb") as f:
+    m.write(f)
