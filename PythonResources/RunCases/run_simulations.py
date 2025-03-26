dda9ae76ce9d1dfbd6cc7c632315b7d30e44d9aa
hcasperfu <hcasperfu@lbl.gov>
2025-03-25 21:45:09 -0700

    added case specification for ThermalGridJBA.Networks.Validation.FiveHubsPlantMultiFlow

--- PythonResources/RunCases/run_simulations.py
+++ PythonResources/RunCases/run_simulations.py
@@ -19,6 +19,7 @@ CASE_LIST = 'fivehubsnoplant'
         eachbuilding: each building, differentiating with or without DHW
         eachcluster: each of the five clusters, all with DHW
         fivehubsnoplant: runs ThermalGridJBA.Networks.Validation.SinglePlantFiveHubs
+        fivehubsmultiflow: runs ThermalGridJBA.Networks.Validation.FiveHubsPlantMultiFlow
 """
 CASE_SPECS = {
      'start_time' : 90 * 24 * 3600,
