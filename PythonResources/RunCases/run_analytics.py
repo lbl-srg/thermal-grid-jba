bfc3f211ee1a359a1f405d78671f6b095996ea83
hcasperfu <hcasperfu@lbl.gov>
2025-03-31 16:30:16 -0700

    added unit support

--- PythonResources/RunCases/run_analytics.py
+++ PythonResources/RunCases/run_analytics.py
@@ -15,36 +15,56 @@ CWD = os.getcwd()
 MAT_FILE_NAME = "ConnectedETSNoDHW_futu.mat"
 mat_file_path = os.path.realpath(os.path.join(CWD, "simulations", MAT_FILE_NAME))
 
+units =    [
+                {'quantity' : 'power',
+                 'unit'     : uy.W,
+                 'displayUnit' : uy.kW
+                 },
+                {'quantity' : 'energy',
+                 'unit'     : uy.J,
+                 'displayUnit' : uy.MWh
+                 }
+            ]
+
 variables = [
                 {'name' : 'EChi.u',
                  'desc' : 'ETS heat recovery chiller electric power input',
-                 'unit' : uy.W,
-                 'actions' : ['max', 'plot']
+                 'quantity' : 'power',
+                 'actions'  : ['max', 'plot'],
+                 'captions' : ['Peak heat recovery chiller electric power input',
+                               'Heat recovery chiller electric power input']
                  },
                 {'name' : 'EChi.y',
                  'desc' : 'ETS heat recovery chiller electrical energy consumption',
-                 'unit' : uy.J,
-                 'actions' : ['last']
+                 'quantity' : 'energy',
+                 'actions'  : ['last'],
+                 'captions' : ['Total heat recovery chiller electrical consumption']
                  },
                 {'name' : 'bui.bui.QReqHea_flow',
                  'desc' : 'Space heating demand at the coil',
-                 'unit' : uy.W,
-                 'actions' : ['max', 'plot']
+                 'quantity' : 'power',
+                 'actions'  : ['max', 'plot'],
+                 'captions' : ['Peak space heating load',
+                               'Space heating load']
                  },
                 {'name' : 'bui.bui.QReqCoo_flow',
                  'desc' : 'Space cooling demand at the coil',
-                 'unit' : uy.W,
-                 'actions' : ['min', 'plot']
+                 'quantity' : 'power',
+                 'actions'  : ['min', 'plot'],
+                 'captions' : ['Peak cooling load',
+                               'Cooling load']
                  },
                 {'name' : 'dHHeaWat.y',
                  'desc' : 'Space heating load at the coil',
-                 'unit' : uy.J,
-                 'actions' : ['last']
+                 'quantity' : 'energy',
+                 'actions'  : ['last'],'unit' : uy.J,
+                 'captions' : ['Total space heating load']
                  },
                 {'name' : 'dHChiWat.y',
                  'desc' : 'Space cooling load at the coil',
-                 'unit' : uy.J,
-                 'actions' : ['last']
+                 'quantity' : 'energy',
+                 'actions'  : ['last'],
+                 'captions' : ['Total space cooling load']
                  },
             ]
 
@@ -66,13 +86,23 @@ def find_var(n, print_message = True):
         
     return y
 
+def str_with_unit(value, quantity):
+    """
+    """
+    u = next((item for item in units if item.get('quantity') == quantity), None)
+    return (value * u['unit']).to(u['displayUnit'])
+    
+
 for var in variables:
     y = find_var(var['name'])
     if len(y):
         if 'max' in var['actions']:
-            print(max(y))
+            v = max(y)
         if 'min' in var['actions']:
-            print(min(y))
+            v = min(y)
         if 'last' in var['actions']:
-            print(y[-1])
+            v = y[-1]
+        vstr = str_with_unit(v, var['quantity'])
+        msg = f"{var['captions'][0]}: {vstr:,.0f}"
+        print(msg)
 
