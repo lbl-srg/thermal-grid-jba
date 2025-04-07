5a910521836730d67879c53a3e44ae15764472e0
hcasperfu <hcasperfu@lbl.gov>
2025-04-07 12:35:38 -0700

    added scenario handling

--- PythonResources/RunCases/run_analytics.py
+++ PythonResources/RunCases/run_analytics.py
@@ -15,6 +15,8 @@ CWD = os.getcwd()
 MAT_FILE_NAME = "ConnectedETSNoDHW_futu.mat"
 PRINT_ACTION_NOT_FOUND = False
 
+#%%
+
 mat_file_path = os.path.realpath(os.path.join(CWD, "simulations", MAT_FILE_NAME))
 
 units =    [
@@ -75,8 +77,22 @@ actions = {'max': max,
            'last': lambda y: y[-1]
            }
 
-
-r=Reader(mat_file_path, "dymola")
+# the first scenario will be the baseline to be compared against
+### replace this with a list of dict?
+scenarios = [
+                {'name'     : 'fTMY',
+                 'mat_file' : 'ConnectedETSNoDHW_futu.mat',
+                 'results'  : {}
+                 },
+                {'name'     : 'Heat wave',
+                 'mat_file' : 'ConnectedETSNoDHW_heat.mat',
+                 'results'  : {}
+                 },
+                {'name'     : 'Cold snap',
+                 'mat_file' : 'ConnectedETSNoDHW_cold.mat',
+                 'results'  : {}
+                 }
+            ]
 
 def find_var(n, print_message = True):
     """ Find the exact var name in results.
@@ -98,18 +114,30 @@ def str_with_unit(value, quantity):
     """
     u = next((item for item in units if item.get('quantity') == quantity), None)
     return (value * u['unit']).to(u['displayUnit'])
-    
-for var in variables:
-    y = find_var(var['name'])
-    if len(y):
-        for action in var['actions']:
-            if action in actions:
-                v = actions[action](y)
-                vstr = str_with_unit(v, var['quantity'])
-                msg = f"{var['captions'][0]}: {vstr:,.0f}"
-                print(msg)
-            else:
-                msg = f'## The action "{action}" is not defined for the varialbe "{var["name"]}"'
-                if PRINT_ACTION_NOT_FOUND:
+
+#%%
+for i, scenario in enumerate(scenarios):
+    mat_file_path = os.path.realpath(os.path.join(CWD, "simulations", scenario['mat_file']))
+    r=Reader(mat_file_path, 'dymola')
+    print(f'Scenario: {scenario["name"]}')
+    for var in variables:
+        y = find_var(var['name'])
+        if len(y):
+            scenario['results'][var['name']] = {}
+            for action in var['actions']:
+                if action in actions:
+                    v = actions[action](y)
+                    vstr = str_with_unit(v, var['quantity'])
+                    msg = ' '*4+f"{var['captions'][0]}: {vstr:,.0f}"
                     print(msg)
+                    
+                    scenario['results'][var['name']][action] = v
+                    # if i > 0:
+                    #     baseline_value = scenario_results[scenarios[0]][var['name']][action]
+                    #     comparison_msg = f"Comparison with baseline ({scenarios[0]}): {var['captions'][0]}: {vstr:,.0f} vs {str_with_unit(baseline_value, var['quantity']):,.0f}"
+                    #     print(comparison_msg)
+                else:
+                    msg = f'## The action "{action}" is not defined for the varialbe "{var["name"]}"'
+                    if PRINT_ACTION_NOT_FOUND:
+                        print(msg)
 
