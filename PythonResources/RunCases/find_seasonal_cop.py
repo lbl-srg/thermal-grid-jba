50f66164e68c844780d8b39c1989e17dd1b5cfe3
hcasperfu <hcasperfu@lbl.gov>
2025-05-01 18:51:07 -0700

    added avg temp to seasonal COP computation for reference

--- PythonResources/RunCases/find_seasonal_cop.py
+++ PythonResources/RunCases/find_seasonal_cop.py
@@ -17,12 +17,13 @@ CWD = os.getcwd()
 PRINT_RESULTS = False
 WRITE_TO_XLSX = True
 PATH_XLSX = os.path.join(CWD, "seasonal_cop.xlsx")
-CASE_LIST = ["ETS_All_futu",
-            "cluster_A_futu",
-            "cluster_B_futu",
-            "cluster_C_futu",
-            "cluster_D_futu",
-            "cluster_E_futu"]
+# CASE_LIST = ["ETS_All_futu",
+#             "cluster_A_futu",
+#             "cluster_B_futu",
+#             "cluster_C_futu",
+#             "cluster_D_futu",
+#             "cluster_E_futu"]
+CASE_LIST = [os.path.join("seasonal_cop", "ETS_All_futu")]
 
 #%%
 if WRITE_TO_XLSX:
@@ -38,8 +39,19 @@ for cas in CASE_LIST:
     (t, COP) = r.values('bui.ets.chi.chi.COP')
     (t, uCoo) = r.values('bui.ets.chi.uCoo')
     (t, uHea) = r.values('bui.ets.chi.uHea')
+    (t, TEvaEnt) = r.values('bui.ets.chi.senTEvaEnt.T')
+    (t, TEvaLvg) = r.values('bui.ets.chi.senTEvaLvg.T')
+    (t, TConEnt) = r.values('bui.ets.chi.senTConEnt.T')
+    (t, TConLvg) = r.values('bui.ets.chi.senTConLvg.T')
     
-    data = pd.DataFrame({'t': t, 'COP': COP, 'uCoo': uCoo, 'uHea': uHea})
+    data = pd.DataFrame({'t': t,
+                         'COP': COP,
+                         'uCoo': uCoo,
+                         'uHea': uHea,
+                         'TEvaEnt' : TEvaEnt,
+                         'TEvaLvg' : TEvaLvg,
+                         'TConEnt' : TConEnt,
+                         'TConLvg' : TConLvg})
     
     # Convert the timestamp to datetime format
     data['datetime'] = pd.to_datetime(data['t'], unit='s', origin='2025-01-01')
@@ -62,22 +74,26 @@ for cas in CASE_LIST:
     modes = ['simultaneous', 'coolingonly', 'heatingonly']
     data['mode'] = np.select(conditions, modes, default='other')
     
-    # Calculate the average of y for each month and each mode
-    result = data.groupby(['month', 'mode'])['COP'].mean().unstack(fill_value=np.nan)
-    result_count = data.groupby(['month', 'mode']).size().unstack(fill_value=0)
-    # result = data.groupby(['month'])['COP'].mean()
+    # Calculate the average of the specified columns by month and mode
+    avg_data = data.groupby(['month', 'mode'])[['COP', 'TEvaEnt', 'TEvaLvg', 'TConEnt', 'TConLvg']].mean().unstack(fill_value=np.nan)
     
-    result.index = result.index.strftime('%B')
-    result_count.index = result_count.index.strftime('%B')
+    # Count the number of occurrences of each month & mode combination
+    count_data = data.groupby(['month', 'mode']).size().unstack(fill_value=0)
+    
+    avg_data.index = avg_data.index.strftime('%B')
+    count_data.index = count_data.index.strftime('%B')
     
     if PRINT_RESULTS:
+        print('='*10)
         print(cas)
-        print(result)
-        print(result_count)
+        print(avg_data)
+        print('')
+        print(count_data)
     
     if WRITE_TO_XLSX:
-        result.to_excel(w, sheet_name=f'{cas}_cop')
-        result_count.to_excel(w, sheet_name=f'{cas}_hours')
+        sheet_name = cas.split(os.sep)[-1]
+        avg_data.to_excel(w, sheet_name=f'{sheet_name}_cop')
+        count_data.to_excel(w, sheet_name=f'{sheet_name}_hours')
 
 if WRITE_TO_XLSX:
     w.close()
