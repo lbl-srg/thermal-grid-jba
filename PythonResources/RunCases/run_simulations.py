72b945b0b37f63daac50799522b89deb8cf74297
JayHuLBL <jianjunhu@lbl.gov>
2025-05-30 15:01:53 -0700

    correct branch

--- PythonResources/RunCases/run_simulations.py
+++ PythonResources/RunCases/run_simulations.py
@@ -4,7 +4,7 @@
 #
 #############################################################
 import os
-BRANCH="main"
+BRANCH="HPSizeParametricStudies"
 SHOW_DYMOLA_GUI = True
 KEEP_DYMOLA_OPEN = False
 FROM_GIT_HUB = True
