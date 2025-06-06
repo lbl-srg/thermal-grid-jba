00473b1402d7497bfd36f841397cc929526ef489
hcasperfu <hcasperfu@lbl.gov>
2025-06-06 11:05:11 -0700

    improved unit expression, choice of parameter

--- PythonResources/RunCases/print_capacity.py
+++ PythonResources/RunCases/print_capacity.py
@@ -15,6 +15,12 @@ import unyt as uy
 #   UnitRegistry once defined. The console must be restarted.
 uy.define_unit("kBTU", 1000 * uy.BTU)
 uy.define_unit("RT", 12000 * uy.Unit("BTU/hr")) # refrigeration ton
+#uy.define_unit("inH2O", 249.089 * uy.Unit("Pa"))
+uy.define_unit("GPM_H2O", 1/15.85 * uy.Unit("kg/s"))
+uy.define_unit("GPM_Glycol", 1/15.85/1.11 * uy.Unit("kg/s"))
+# glycol density = 1.11e3 kg/m3
+uy.define_unit("CFM_Air", 1/1.293*3.28**3*60 * uy.Unit("kg/s"))
+# 1 kg/s / 1.293 kg/m3 * 3.28^3 (ft3/m3) * 60 min/s = 1637 cfm
 
 # CWD = os.getcwd()
 CWD = os.path.dirname(os.path.abspath(__file__))
@@ -72,8 +78,8 @@ def write_row(val,
     
 tab = ""
 tab += "\\begin{tabular}{llrlrl}\n"
-tab += "\\hline\n"
-tab += " & System-wide capacity & \\multicolumn{2}{c}{SI unit} & \\multicolumn{2}{c}{IP unit} \\\\\n"
+tab += "\\toprule\n"
+tab += " & System capacity & \\multicolumn{2}{c}{SI unit} & \\multicolumn{2}{c}{IP unit} \\\\\n"
 tab += "\\hline\n"
 
 ## ETS
@@ -134,19 +140,20 @@ tab += write_row(val = abs(read_parameter('cenPla.gen.heaPum.QCoo_flow_nominal')
                  display_ip = "ton")
 
 # dry cooler
-tab += write_row(val = read_parameter('cenPla.gen.dryCoo.m1_flow_nominal'),
+tab += write_row(val = read_parameter('cenPla.gen.fanDryCoo.m_flow_nominal')/1.293,
                  desc = "Dry cooler - air side",
-                 unit_mat = "kg/s",
-                 unit_si = "kg/s",
-                 unit_ip = "kg/s",
-                 display_ip = "FIXME - cfm")
+                 unit_mat = "m**3/s",
+                 unit_si = "m**3/hr",
+                 unit_ip = "ft**3/min",
+                 display_si = "m3/h",
+                 display_ip = "cfm")
 
-tab += write_row(val = read_parameter('cenPla.gen.dryCoo.m2_flow_nominal'),
+tab += write_row(val = read_parameter('cenPla.gen.pumDryCoo.m_flow_nominal'),
                  desc = "Dry cooler - glycol side",
                  unit_mat = "kg/s",
                  unit_si = "kg/s",
-                 unit_ip = "kg/s",
-                 display_ip = "FIXME - gpm")
+                 unit_ip = "GPM_Glycol",
+                 display_ip = "gpm")
 
 # borefield
 tab += write_row(val = read_max_abs('EBorPer.y'),
@@ -172,15 +179,14 @@ tab += write_row(val = read_parameter('datDis.mPumDis_flow_nominal'),
                  desc = "Distribution pump flow rate",
                  unit_mat = "kg/s",
                  unit_si = "kg/s",
-                 unit_ip = "kg/s",
-                 display_ip = "FIXME - gpm")
+                 unit_ip = "GPM_H2O",
+                 display_ip = "gpm")
 
 tab += write_row(val = read_parameter('pumDis.dp_nominal'),
                  desc = "Distribution pump pressure rise",
                  unit_mat = "Pa",
                  unit_si = "kPa",
-                 unit_ip = "psi",
-                 display_ip = "FIXME - inH2O")
+                 unit_ip = "psi")
 
 # length of pipes
 l = 0
@@ -201,7 +207,7 @@ tab += write_row(val = l,
                  unit_si = "m",
                  unit_ip = "ft")
 
-tab += "\\hline\n"
+tab += "\\bottomrule\n"
 
 tab += "\\end{tabular}"
 
