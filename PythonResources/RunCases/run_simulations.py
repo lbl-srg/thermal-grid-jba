fe5217a3c40628c194f2ed5d9f8f408cd65c38fe
Michael Wetter <mwetter@lbl.gov>
2025-06-26 19:52:18 -0700

    Changed branch to main

--- PythonResources/RunCases/run_simulations.py
+++ PythonResources/RunCases/run_simulations.py
@@ -4,7 +4,7 @@
 #
 #############################################################
 import os
-BRANCH="issue117_tmy3"
+BRANCH="main"
 SHOW_DYMOLA_GUI = False
 KEEP_DYMOLA_OPEN = False
 FROM_GIT_HUB = True
