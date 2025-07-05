ea861eea99678cea8d8d1edf849f304b83b9ffcd
hcasperfu <hcasperfu@lbl.gov>
2025-07-04 21:28:12 -0700

    added awhp results to table, improved code structure

--- PythonResources/RunCases/compare_weather_scenarios.py
+++ PythonResources/RunCases/compare_weather_scenarios.py
@@ -10,42 +10,87 @@ import os
 import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
-from GetVariables import get_vars, index_var_list, integrate_with_condition
+from GetVariables import *
 # python file under same folder
 
 #CWD = os.getcwd()
 CWD = os.path.dirname(os.path.abspath(__file__))
-mat_file_base = os.path.join(CWD, "simulations", "2025-06-09_weatherscenarios", "DetailedPlantFiveHubs-001.mat")
-mat_file_heat = os.path.join(CWD, "simulations", "2025-06-09_weatherscenarios", "DetailedPlantFiveHubs-004.mat")
-mat_file_cold = os.path.join(CWD, "simulations", "2025-06-09_weatherscenarios", "DetailedPlantFiveHubs-003.mat")
+mat_file = dict()
+mat_file['ets'] = dict()
+mat_file['ets']['base'] = os.path.join(CWD, "simulations", "2025-06-09_weatherscenarios", "DetailedPlantFiveHubs-001.mat")
+mat_file['ets']['heat'] = os.path.join(CWD, "simulations", "2025-06-09_weatherscenarios", "DetailedPlantFiveHubs-004.mat")
+mat_file['ets']['cold'] = os.path.join(CWD, "simulations", "2025-06-09_weatherscenarios", "DetailedPlantFiveHubs-003.mat")
+mat_file['awhp'] = dict()
+mat_file['awhp']['base'] = os.path.join(CWD, "simulations", "awhp", "AirToWater-base.mat")
+mat_file['awhp']['heat'] = os.path.join(CWD, "simulations", "awhp", "AirToWater-heat.mat")
+mat_file['awhp']['cold'] = os.path.join(CWD, "simulations", "awhp", "AirToWater-cold.mat")
 
 J_to_kWh = 1 / 3600 * 1e-3
 J_to_MWh = 1 / 3600 * 1e-6
 J_to_MMBtu = 1 / 1.05505585262e9
 
-#%% construct var list
-_i = '%%i%%'
-nBui = 5
+_i = '%%i%%' # index placeholder
+
+#%% construct results dfs
+results = dict()
+models = ['ets', 'awhp']
+events = ['base', 'heat', 'cold']
+
+# ets
+model = 'ets'
+nBui = 5 # will be checked against 'nBui' from the mat file
 
 var_list = list()
-var_list += ['ETot.y',      # Total ele consumption, J
-             'EHeaPum.y',   # ETS compressor ele consumption, J
-             'EComPla.y',   # Plant compressor ele consumption, J
-             'EPumETS.y',   # ETS pump ele consumption, J
-             'EPumPla.y']   # Plant pump ele consumption, J
-var_pre_index = [f'bui[{_i}].bui.loa.y[1]', # Building cooling load, W
-                 f'bui[{_i}].bui.loa.y[2]', # Building sp. heating load, W
-                 f'bui[{_i}].bui.terUniCoo.TLoaODE.TAir', # Room temp for cooling, K
-                 f'bui[{_i}].bui.terUniHea.TLoaODE.TAir'] # Room temp for heating, K
+var_list += ['nBui',        # number of buildings, for verification
+             'cenPla.gen.heaPum.QCon_flow', # plant nominal condenser output, W
+             'cenPla.gen.heaPum.QEva_flow', # plant nominal evaporator output, W
+             'cenPla.gen.heaPum.P',         # plant compressor power, W
+             'cenPla.gen.heaPum.hea',       # heating = 1, cooling = 0
+             ]
 
+var_pre_index = [f'bui[{_i}].ets.heaPum.con.uCoo',  # cooling mode
+                 f'bui[{_i}].ets.heaPum.con.hea.y', # heating mode, including dhw
+                 f'bui[{_i}].bui.loa.y[1]', # building cooling load, W
+                 f'bui[{_i}].bui.loa.y[2]', # building sp. heating load, W
+                 f'bui[{_i}].ets.heaPum.heaPum.QCon_flow', # ets condenser output, W
+                 f'bui[{_i}].ets.heaPum.heaPum.P' # ets compressor power, W
+                 ]
 var_list += index_var_list(var_pre_index,
                            _i,
                            range(1, nBui+1))
 
-#%% get vars
-results_base = get_vars(var_list, mat_file_base, 'dymola')
-results_heat = get_vars(var_list, mat_file_heat, 'dymola')
-results_cold = get_vars(var_list, mat_file_cold, 'dymola')
+results[model] = dict()
+for key in events:
+    results[model][key] = get_vars(var_list, mat_file[model][key], 'dymola')
+
+assert nBui == results['ets']['base']['nBui'].iloc[0], """
+Error: For the ETS model,
+`nBui` from the python code and `nBui` from the baseline mat file do not match.
+Execution aborted."""
+
+# awhp
+model = 'awhp'
+nHp = 3 # will be checked against 'pla.nHp' from the mat file
+
+var_list = list()
+var_list += ['pla.nHp'] # number of heat pumps, for verification
+var_pre_index = [f'pla.hp.hp[{_i}].hp.uMod',        # mode, cooling -1, heating +1, off 0
+                 f'pla.hp.hp[{_i}].hp.QLoa_flow',   # load side heat flow
+                 f'pla.hp.hp[{_i}].hp.QSou_flow',   # source side heat flow
+                 f'pla.hp.hp[{_i}].hp.P',           # compressor power
+                 ]
+var_list += index_var_list(var_pre_index,
+                           _i,
+                           range(1, nHp+1))
+
+results[model] = dict()
+for key in events:
+    results[model][key] = get_vars(var_list, mat_file[model][key], 'dymola')
+
+assert nHp == results['awhp']['base']['pla.nHp'].iloc[0], """
+Error: For the AWHP model,
+`nHp` from the python code and `pla.nHp` from the baseline mat file do not match.
+Execution aborted."""
 
 #%% get time stamps in seconds
 def soy(dt):
@@ -57,26 +102,26 @@ def soy(dt):
 # start and end dates of the weather events, both dates inclusive
 # see PythonResources/Data/Consumption/Anemoi.py
 _year = 2025 # dummy year, has no effect
-sec_heat_from = soy(datetime(_year, 7, 27))
-sec_heat_to   = soy(datetime(_year, 8,  9)) + 24*3600
-sec_cold_from = soy(datetime(_year, 2, 23))
-sec_cold_to   = soy(datetime(_year, 3,  8)) + 24*3600
+duration = {event: {} for event in ['heat', 'cold']}
+duration['heat'] = [soy(datetime(_year, 7, 27)),            # 17884800
+                    soy(datetime(_year, 8,  9)) + 24*3600   # 19008000
+                    ]
+duration['cold'] = [soy(datetime(_year, 2, 23)),            #  4579200
+                    soy(datetime(_year, 3,  8)) + 24*3600   #  5702400
+                    ]
 
 #%% Compute and compare variables
-def get_section(df, sec_from, sec_to):
+def copy_section(df, duration):
     """ Make a copy of the dataframe where
-            df['Time'] >= sec_from and df['Time'] < sec_to
+            df['Time'] >= duration[0] and df['Time'] < duration[1]
     """
-    df_section = df[(df['Time'] >= sec_from) & (df['Time'] < sec_to)].copy()
+    df_section = df[(df['Time'] >= duration[0]) &
+                    (df['Time'] < duration[1])].copy()
     
     return df_section
 
-def write_latex_table(df_base,
-                      df_even,
-                      event : str):
-    """ df_base : baseline
-        df_even : event
-        event : ['heat', 'cold']
+def write_latex_table(event : str):
+    """ event : ['heat', 'cold']
     """
     
     def write_row(description,
@@ -84,9 +129,12 @@ def write_latex_table(df_base,
                   v_even,
                   unit_si : str,
                   factor_si,
+                  format_si = ',.0f',
                   to_ip = False,
                   unit_ip = None,
-                  factor_ip = None):
+                  format_ip = ',.0f',
+                  factor_ip = None,
+                  skip_compare = False):
         """ Returns one row of the latex table if ip unit not needed,
               otherwise returns also a second row in ip unit.
             description : first column,
@@ -97,29 +145,97 @@ def write_latex_table(df_base,
             to_ip : boolean, if true returns second row in ip unit,
             unit_ip : display unit string for ip,
             factor_ip : conversion factor for ip.
+            skip_compare : skip printing the percentage
         """
         
         tab = ""
         
         v_base_si = v_base * factor_si
         v_even_si = v_even * factor_si
-        diff_v_si = v_even_si - v_base_si
-        if abs(v_base_si) < 1e-10:
+        if skip_compare:
+            diff_v_si = ""
+        else:
+            _diff_v_si = v_even_si - v_base_si
+            diff_v_si = f'{_diff_v_si:+{format_si}}'
+        if abs(v_base_si) < 1e-10 or skip_compare:
             diff_p_str = ""
         else:
-            diff_p_str = f'{diff_v_si/v_base_si*100:+.1f}\\%'
+            _diff_p = _diff_v_si/v_base_si*100
+            diff_p_str = f'{_diff_p:+.3g}\\%'
         
-        tab += f"{description} & [{unit_si}] & {v_base_si:,.0f} & {v_even_si:,.0f} & \\textit{{{diff_v_si:+,.0f}}} & \\textit{{{diff_p_str}}} \\\\\n"
+        tab += f"{description} & [{unit_si}] & {v_base_si:{format_si}} & {v_even_si:{format_si}} & \\textit{{{diff_v_si}}} & \\textit{{{diff_p_str}}} \\\\\n"
         
         if to_ip:
             v_base_ip = v_base * factor_ip
             v_even_ip = v_even * factor_ip
-            diff_v_ip = v_even_ip - v_base_ip
+            if skip_compare:
+                diff_v_ip = ""
+            else:
+                _diff_v_ip = v_even_ip - v_base_ip
+                diff_v_ip = f'{_diff_v_ip:+{format_ip}}'
             
-            tab += f" & [{unit_ip}] & {v_base_ip:,.0f} & {v_even_ip:,.0f} & \\textit{{{diff_v_ip:+,.0f}}} & \\\\\n"
+            tab += f" & [{unit_ip}] & {v_base_ip:{format_ip}} & {v_even_ip:{format_ip}} & \\textit{{{diff_v_ip}}} & \\\\\n"
             
         return tab
     
+    scenarios = ['base', event]
+    
+    # copy sub-dataframes for the duration of the event for each model each scenario
+    dfs = {model:
+                {sce: copy_section(results[model][sce], duration[event])
+                      for sce in scenarios}
+                for model in models}
+    # energy and cop computation
+    QCon = {model:
+                 {sce: 0. for sce in scenarios}
+                          for model in models}
+    PEle = {model:
+                 {sce: 0. for sce in scenarios}
+                          for model in models}
+    COP  = {model:
+                 {sce: 0. for sce in scenarios}
+                          for model in models}
+    for sce in scenarios:
+        model = 'ets'
+        # each ets
+        for i in range(1, nBui+1):
+            if event == 'heat':
+                condition = np.array(dfs[model][sce][f'bui[{i}].ets.heaPum.con.uCoo'] == 1)
+            else:
+                condition = np.array(dfs[model][sce][f'bui[{i}].ets.heaPum.con.hea.y'] == 1)
+            QCon[model][sce] += integrate_with_condition(dfs[model][sce], f'bui[{i}].ets.heaPum.heaPum.QCon_flow',
+                                                         condition = condition)
+            PEle[model][sce] += integrate_with_condition(dfs[model][sce], f'bui[{i}].ets.heaPum.heaPum.P',
+                                                         condition = condition)
+        # central plant
+        if event == 'heat':
+            condition = np.array(dfs[model][sce]['cenPla.gen.heaPum.hea'] == 0)
+        else:
+            condition = np.array(dfs[model][sce]['cenPla.gen.heaPum.hea'] == 1)
+        QCon[model][sce] += integrate_with_condition(dfs[model][sce], 'cenPla.gen.heaPum.QCon_flow',
+                                                     sign = 'positive') + \
+                            integrate_with_condition(dfs[model][sce], 'cenPla.gen.heaPum.QEva_flow',
+                                                     sign = 'positive')
+        PEle[model][sce] += integrate_with_condition(dfs[model][sce], 'cenPla.gen.heaPum.P',
+                                                     condition = condition)
+        COP[model][sce] = QCon[model][sce] / PEle[model][sce]
+            
+        model = 'awhp'
+        cols = [f'pla.hp.hp[{_i}].hp.P' for _i in range(1, nHp+1)]
+        for i in range(1, nHp+1):
+            if event == 'heat':
+                condition = np.array(dfs[model][sce][f'pla.hp.hp[{i}].hp.uMod'] == -1)
+            else:
+                condition = np.array(dfs[model][sce][f'pla.hp.hp[{i}].hp.uMod'] == 1)
+            # because the hp is reversible, the condenser is whichever side is positive
+            QCon[model][sce] += integrate_with_condition(dfs[model][sce], f'pla.hp.hp[{i}].hp.QLoa_flow',
+                                                         condition = condition, sign = 'positive') + \
+                                integrate_with_condition(dfs[model][sce], f'pla.hp.hp[{i}].hp.QSou_flow',
+                                                         condition = condition, sign = 'positive')
+            PEle[model][sce] += integrate_with_condition(dfs[model][sce], f'pla.hp.hp[{i}].hp.P',
+                                                         condition = condition)
+        COP[model][sce] = QCon[model][sce] / PEle[model][sce]
+        
     if event == 'heat':
         event_str = 'Heat wave'
     else:
@@ -130,60 +246,72 @@ def write_latex_table(df_base,
     tab += "% *remarks*\n\n"
     tab += "\\begin{tabular}{lrrrrr}\n"
     tab += "\\toprule\n"
-    tab += f"During the event & & Baseline & {event_str} & & \\\\\n"
+    tab += f" & & Baseline & {event_str} & & \\\\\n"
     tab += "\\hline\n"
     
     # main body
     
+    # end-use heating or cooling load
     if event == 'heat':
-        # load
-        cols_coo = [f'bui[{_i}].bui.loa.y[1]' for _i in range(1, nBui+1)]
-        df_base['CooLoa'] = df_base[cols_coo].sum(axis=1)
-        df_even['CooLoa'] = df_even[cols_coo].sum(axis=1)
-        
-        tab += write_row(description = "End-use cooling load",
-                         v_base = abs(integrate_with_condition(df_base, 'CooLoa')),
-                         v_even = abs(integrate_with_condition(df_even, 'CooLoa')),
-                         unit_si = 'MWh',
-                         factor_si = J_to_MWh,
-                         to_ip = True,
-                         unit_ip = 'MMBtu',
-                         factor_ip = J_to_MMBtu)
+        desc = "End-use cooling load"
+        cols = [f'bui[{_i}].bui.loa.y[1]' for _i in range(1, nBui+1)]
+    else:
+        desc = "End-use heating load"
+        cols = [f'bui[{_i}].bui.loa.y[2]' for _i in range(1, nBui+1)]
+    for sce in scenarios:
+        dfs['ets'][sce]['Load'] = dfs['ets'][sce][cols].sum(axis=1)
+    tab += write_row(description = desc,
+                     v_base = abs(integrate_with_condition(dfs['ets']['base'], 'Load')),
+                     v_even = abs(integrate_with_condition(dfs['ets'][event], 'Load')),
+                     unit_si = 'MWh',
+                     factor_si = J_to_MWh,
+                     to_ip = True,
+                     unit_ip = 'MMBtu',
+                     factor_ip = J_to_MMBtu)
     
-    if event == 'cold':
-        # load
-        cols_hea = [f'bui[{_i}].bui.loa.y[2]' for _i in range(1, nBui+1)]
-        df_base['HeaLoa'] = df_base[cols_hea].sum(axis=1)
-        df_even['HeaLoa'] = df_even[cols_hea].sum(axis=1)
-        
-        tab += write_row(description = "End-use space heating load",
-                         v_base = integrate_with_condition(df_base, 'HeaLoa'),
-                         v_even = integrate_with_condition(df_even, 'HeaLoa'),
-                         unit_si = 'MWh',
-                         factor_si = J_to_MWh,
-                         to_ip = True,
-                         unit_ip = 'MMBtu',
-                         factor_ip = J_to_MMBtu)
+    # ets
+    tab += "ETS system & & & & & \\\\\n"
+    model = 'ets'
     
-    # compressor load
-    tab += write_row(description = "Compressor electricity use",
-                     v_base = df_base['EHeaPum.y'].iloc[-1] - df_base['EHeaPum.y'].iloc[0]
-                            + df_base['EComPla.y'].iloc[-1] - df_base['EComPla.y'].iloc[0],
-                     v_even = df_even['EHeaPum.y'].iloc[-1] - df_even['EHeaPum.y'].iloc[0]
-                            + df_even['EComPla.y'].iloc[-1] - df_even['EComPla.y'].iloc[0],
+    # ets compressor load
+    tab += write_row(description = r"\hspace{0.5cm}" + "System-wide compressor load",
+                     v_base = PEle[model]['base'],
+                     v_even = PEle[model][event],
                      unit_si = 'MWh',
                      factor_si = J_to_MWh,
                      to_ip = False)
     
-    # pump load
-    tab += write_row(description = "Pump electricity use",
-                     v_base = df_base['EPumETS.y'].iloc[-1] - df_base['EPumETS.y'].iloc[0]
-                            + df_base['EPumPla.y'].iloc[-1] - df_base['EPumPla.y'].iloc[0],
-                     v_even = df_even['EPumETS.y'].iloc[-1] - df_even['EPumETS.y'].iloc[0]
-                            + df_even['EPumPla.y'].iloc[-1] - df_even['EPumPla.y'].iloc[0],
+    # ets COP
+    
+    tab += write_row(description = r"\hspace{0.5cm}" + "Average COP",
+                     v_base = COP[model]['base'],
+                     v_even = COP[model][event],
+                     unit_si = '-',
+                     factor_si = 1.,
+                     format_si = '.2f',
+                     skip_compare = True)
+    
+    # awhp# ets
+    tab += "AWHP system & & & & & \\\\\n"
+    model = 'awhp'
+    
+    # awhp compressor load
+    
+    tab += write_row(description = r"\hspace{0.5cm}" + "System-wide compressor load",
+                     v_base = PEle[model]['base'],
+                     v_even = PEle[model][event],
                      unit_si = 'MWh',
                      factor_si = J_to_MWh,
                      to_ip = False)
+    
+    # awhp COP
+    tab += write_row(description = r"\hspace{0.5cm}" + "Average COP",
+                     v_base = COP[model]['base'],
+                     v_even = COP[model][event],
+                     unit_si = '-',
+                     factor_si = 1.,
+                     format_si = '.2f',
+                     skip_compare = True)
     
     # footer
     tab += "\\bottomrule\n"
@@ -191,17 +319,11 @@ def write_latex_table(df_base,
 
     return tab
 
-# heat wave
-df_heat_base = get_section(results_base, sec_heat_from, sec_heat_to)
-df_heat_even = get_section(results_heat, sec_heat_from, sec_heat_to)
+#%%
+if __name__ == "__main__":
 
-tab_heat = write_latex_table(df_base = df_heat_base,
-                             df_even = df_heat_even,
-                             event = 'heat')
-
-# cold snap
-df_cold_base = get_section(results_base, sec_cold_from, sec_cold_to)
-df_cold_even = get_section(results_cold, sec_cold_from, sec_cold_to)
-tab_cold = write_latex_table(df_base = df_cold_base,
-                             df_even = df_cold_even,
-                             event = 'cold')
+    # heat wave
+    tab_heat = write_latex_table('heat')
+    
+    # cold snap
+    tab_cold = write_latex_table('cold')
